b9385e8dc8c28518b4f50b7dc633b346d9127586
Robert Sebastian Herlim <ngiooong@yahoo.com>
2018-04-03 19:56:44 +0700

    First commit

---
+++ data.js
@@ -0,0 +1,93 @@
+// -- DATA
+var cuisines = [
+    {
+        "id": "british",
+        "x": 886,
+        "y": 173,
+        "probability": {
+            "apple": 0.524,
+            "banana": 0.2712,
+            "strawberry": 0.1252
+        }
+    },
+    {
+        "id": "italian",
+        "x": 1082,
+        "y": 334,
+        "probability": {
+            "apple": 0.1,
+            "banana": 0.2,
+            "strawberry": 0.3
+        }
+    },
+    {
+        "id": "moroccan",
+        "x": 826,
+        "y": 430,
+        "probability": {
+            "apple": 0.0,
+            "banana": 0.0,
+            "strawberry": 0.0
+        }
+    },
+    {
+        "id": "indian",
+        "x": 1379,
+        "y": 474,
+        "probability": {
+            "apple": 0.0,
+            "banana": 0.0,
+            "strawberry": 0.0
+        }
+    },
+    {
+        "id": "chinese",
+        "x": 1519,
+        "y": 276,
+        "probability": {
+            "apple": 0.0,
+            "banana": 0.0,
+            "strawberry": 0.0
+        }
+    },
+    {
+        "id": "korean",
+        "x": 1734,
+        "y": 332,
+        "probability": {
+            "apple": 0.0,
+            "banana": 0.0,
+            "strawberry": 0.0
+        }
+    },
+    {
+        "id": "thai",
+        "x": 1607,
+        "y": 562,
+        "probability": {
+            "apple": 0.0,
+            "banana": 0.0,
+            "strawberry": 0.0
+        }
+    },
+    {
+        "id": "brazilian",
+        "x": 632,
+        "y": 679,
+        "probability": {
+            "apple": 0.0,
+            "banana": 0.0,
+            "strawberry": 0.0
+        }
+    },
+    {
+        "id": "mexican",
+        "x": 275,
+        "y": 470,
+        "probability": {
+            "apple": 0.0,
+            "banana": 0.0,
+            "strawberry": 0.0
+        }
+    }
+]
