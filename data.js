93603a6ef2a7bb1edff0525f5cc7a79c01c39c6a
Robert Sebastian Herlim <ngiooong@yahoo.com>
2018-04-04 20:38:17 +0700

    Add icon to filter dropdown

--- data.js
+++ data.js
@@ -1,93 +1,117 @@
 // -- DATA
 var cuisines = [
     {
-        "id": "british",
-        "x": 886,
-        "y": 173,
-        "probability": {
-            "apple": 0.524,
-            "banana": 0.2712,
-            "strawberry": 0.1252
+        id: "british",
+        img: "public/__default.png",
+        x: 886,
+        y: 173,
+        probability: {
+            apple: 0.524,
+            banana: 0.2712,
+            strawberry: 0.1252
         }
     },
     {
-        "id": "italian",
-        "x": 1082,
-        "y": 334,
-        "probability": {
-            "apple": 0.1,
-            "banana": 0.2,
-            "strawberry": 0.3
+        id: "italian",
+        img: "public/__default.png",
+        x: 1082,
+        y: 334,
+        probability: {
+            apple: 0.1,
+            banana: 0.2,
+            strawberry: 0.3
         }
     },
     {
-        "id": "moroccan",
-        "x": 826,
-        "y": 430,
-        "probability": {
-            "apple": 0.0,
-            "banana": 0.0,
-            "strawberry": 0.0
+        id: "moroccan",
+        img: "public/__default.png",
+        x: 826,
+        y: 430,
+        probability: {
+            apple: 0.0,
+            banana: 0.0,
+            strawberry: 0.0
         }
     },
     {
-        "id": "indian",
-        "x": 1379,
-        "y": 474,
-        "probability": {
-            "apple": 0.0,
-            "banana": 0.0,
-            "strawberry": 0.0
+        id: "indian",
+        img: "public/__default.png",
+        x: 1379,
+        y: 474,
+        probability: {
+            apple: 0.0,
+            banana: 0.0,
+            strawberry: 0.0
         }
     },
     {
-        "id": "chinese",
-        "x": 1519,
-        "y": 276,
-        "probability": {
-            "apple": 0.0,
-            "banana": 0.0,
-            "strawberry": 0.0
+        id: "chinese",
+        img: "public/__default.png",
+        x: 1519,
+        y: 276,
+        probability: {
+            apple: 0.0,
+            banana: 0.0,
+            strawberry: 0.0
         }
     },
     {
-        "id": "korean",
-        "x": 1734,
-        "y": 332,
-        "probability": {
-            "apple": 0.0,
-            "banana": 0.0,
-            "strawberry": 0.0
+        id: "korean",
+        img: "public/__default.png",
+        x: 1734,
+        y: 332,
+        probability: {
+            apple: 0.0,
+            banana: 0.0,
+            strawberry: 0.0
         }
     },
     {
-        "id": "thai",
-        "x": 1607,
-        "y": 562,
-        "probability": {
-            "apple": 0.0,
-            "banana": 0.0,
-            "strawberry": 0.0
+        id: "thai",
+        img: "public/__default.png",
+        x: 1607,
+        y: 562,
+        probability: {
+            apple: 0.0,
+            banana: 0.0,
+            strawberry: 0.0
         }
     },
     {
-        "id": "brazilian",
-        "x": 632,
-        "y": 679,
-        "probability": {
-            "apple": 0.0,
-            "banana": 0.0,
-            "strawberry": 0.0
+        id: "brazilian",
+        img: "public/__default.png",
+        x: 632,
+        y: 679,
+        probability: {
+            apple: 0.0,
+            banana: 0.0,
+            strawberry: 0.0
         }
     },
     {
-        "id": "mexican",
-        "x": 275,
-        "y": 470,
-        "probability": {
-            "apple": 0.0,
-            "banana": 0.0,
-            "strawberry": 0.0
+        id: "mexican",
+        img: "public/__default.png",
+        x: 275,
+        y: 470,
+        probability: {
+            apple: 0.0,
+            banana: 0.0,
+            strawberry: 0.0
         }
+    }
+]
+
+var ingredients = [
+    {
+        id: "apple",
+        img: "public/__default.png",
+    },
+    {
+        id: "banana",
+        img: "public/__default.png",
+    },
+    {
+        id: "strawberry",
+        img: "public/__default.png",
     }
 ]
