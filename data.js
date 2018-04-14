f3afdfefa0eddd09f9bcda5a41239c1d18dcaa7b
elvinvina <vina9196@gmail.com>
2018-04-14 23:03:16 +0700

    ingredient 50%

--- data.js
+++ data.js
@@ -6,11 +6,14 @@ var cuisines = [
         x: 886,
         y: 173,
         probability: {
-            apple: 0.524,
-            banana: 0.2712,
-            strawberry: 0.1252
+            flour: 0.555,
+            egg: 0.52,
+            cream: 0.225,
+            milk: 0.375,
+            potato: 0.175,
+            cheese: 0.125
         },
-        worldmapDisplayedIngredients: ['apple', 'banana', 'strawberry', 'strawberry', 'apple', 'banana']
+        worldmapDisplayedIngredients: ['flour', 'egg', 'milk', 'cream', 'potato', 'cheese']
     },
     {
         id: "italian",
@@ -18,11 +21,14 @@ var cuisines = [
         x: 1082,
         y: 334,
         probability: {
-            apple: 0.1,
-            banana: 0.2,
-            strawberry: 0.3
+            cheese: 0.545,
+            olive: 0.435,
+            tomato: 0.305,
+            basil: 0.21,
+            lemon: 0.2,
+            egg: 0.145
         },
-        worldmapDisplayedIngredients: ['apple', 'banana', 'strawberry', 'strawberry', 'apple', 'banana']
+        worldmapDisplayedIngredients: ['cheese', 'olive', 'tomato', 'basil', 'lemon', 'egg']
     },
     {
         id: "moroccan",
@@ -30,11 +36,14 @@ var cuisines = [
         x: 826,
         y: 430,
         probability: {
-            apple: 0.0,
-            banana: 0.0,
-            strawberry: 0.0
+            olive: 0.55,
+            lemon: 0.41,
+            ginger: 0.315,
+            tomato: 0.34,
+            paprika: 0.275,
+            carrot: 0.23,
         },
-        worldmapDisplayedIngredients: ['apple', 'banana', 'strawberry', 'strawberry', 'apple', 'banana']
+        worldmapDisplayedIngredients: ['olive', 'lemon', 'ginger', 'tomato', 'paprika', 'carrot']
     },
     {
         id: "indian",
@@ -42,11 +51,14 @@ var cuisines = [
         x: 1379,
         y: 474,
         probability: {
-            apple: 0.0,
-            banana: 0.0,
-            strawberry: 0.0
+            ginger: 0.505,
+            tomato: 0.435,
+            mustard: 0.225,
+            lemon: 0.2,
+            coconut: 0.175,
+            potato: 0.155
         },
-        worldmapDisplayedIngredients: ['apple', 'banana', 'strawberry', 'strawberry', 'apple', 'banana']
+        worldmapDisplayedIngredients: ['ginger', 'tomato', 'mustard', 'lemon', 'coconut', 'potato']
     },
     {
         id: "chinese",
@@ -111,6 +123,66 @@ var cuisines = [
 ]
 
 var ingredients = [
+    {
+        id: "flour",
+        img: "public/ing/flour.png",
+    },
+    {
+        id: "tomato",
+        img: "public/ing/tomato.png",
+    },
+    {
+        id: "carrot",
+        img: "public/ing/carrot.png",
+    },
+    {
+        id: "egg",
+        img: "public/ing/egg.png",
+    },
+    {
+        id: "mustard",
+        img: "public/ing/mustard.png",
+    },
+    {
+        id: "lemon",
+        img: "public/ing/lemon.png",
+    },
+    {
+        id: "basil",
+        img: "public/ing/basil.png",
+    },
+    {
+        id: "milk",
+        img: "public/ing/milk.png",
+    },
+    {
+        id: "cream",
+        img: "public/ing/cream.png",
+    },
+    {
+        id: "potato",
+        img: "public/ing/potato.png",
+    },
+    {
+        id: "ginger",
+        img: "public/ing/ginger.png",
+    },
+    {
+        id: "coconut",
+        img: "public/ing/coconut.png",
+    },
+    {
+        id: "cheese",
+        img: "public/ing/cheese.png",
+    },
+    {
+        id: "olive",
+        img: "public/ing/olive.png",
+    },
+    {
+        id: "paprika",
+        img: "public/ing/paprika.png",
+    },
     {
         id: "apple",
         img: "public/__default.png",
